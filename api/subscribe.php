a8ce5bcc69d1aae8b9a0998294e85732b809f634
simon czarneki <multymine988@gmail.com>
2026-05-16 23:26:09 +0200

    Fix subscribe: save subscribers.txt in api/ directory

--- api/subscribe.php
+++ api/subscribe.php
@@ -4,8 +4,10 @@ $redirect = $_POST['redirect'] ?? '/';
 
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
   $log = date('c') . ' - ' . $email . PHP_EOL;
-  file_put_contents(__DIR__ . '/../subscribers.txt', $log, FILE_APPEND | LOCK_EX);
-  error_log('[SUBSCRIBE] ' . $email . ' ' . date('c'));
+  $file = __DIR__ . '/subscribers.txt';
+  @file_put_contents($file, $log, FILE_APPEND | LOCK_EX);
+  if (!file_exists($file)) @touch($file);
+  error_log('[SUBSCRIBE] ' . $email . ' saved to ' . $file);
 }
 
 $redirect = preg_replace('/[^a-zA-Z0-9._\/-]/', '', $redirect);
